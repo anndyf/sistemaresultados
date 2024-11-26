6e7aea7049a4fe2fafb29193e2a1910d1e141c1b
anndyf <andressamirella21@gmail.com>
2024-11-26 01:57:07 -0300

    Atualizei os requirements

--- sistema_final/settings.py
+++ sistema_final/settings.py
@@ -72,14 +72,26 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sistema_final.wsgi.application'
 
-
-# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'resultados',  # Substitua pelo nome do banco
+        'USER': 'postgres',       # Usuário criado
+        'PASSWORD': 'admin2024',  # A senha configurada
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'resultados',  # Substitua pelo nome do banco
+        'USER': 'postgres',       # Usuário criado
+        'PASSWORD': 'admin2024',  # A senha configurada
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
